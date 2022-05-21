0bb845b216c277ce88861c06450d070f7f9e454a
Alisson Alves <105590677+aas1989@users.noreply.github.com>
2022-05-21 17:23:22 -0300

    Add files via upload

---
+++ models.js
@@ -0,0 +1,7 @@
+let modelsJson = [
+    {id:1, name:'Doguitos Purina', img:'img/doguitos.jpeg', price:[7.55, 9.90, 14.90], sizes:['1/72', '1/48', '1/32'], description:'Petisco Doguitos Rodízio para Cães Adultos e Filhotes Sabor Carne - 65g'},
+    {id:2, name:'Golden Carne e Arroz', img:'img/golden.jpeg', price:[20.90, 27.90, 33.90], sizes:['1/72', '1/48', '1/32'], description:'Ração Golden Fórmula Mini Bits Para Cães Adultos Pequeno Porte Sabor Carne e Arroz'},
+    {id:3, name:'Muffin PetDog', img:'img/petdog.jpeg', price:[2.99, 4.90, 7.90], sizes:['1/72', '1/48', '1/32'], description:'Bolo Pet Dog Muffin para Cães Sabor Chocolate'},
+    {id:4, name:'Premier Shitzu Frango', img:'img/premier1.jpeg', price:[94.90, 139.90, 199.90], sizes:['1/72', '1/48', '1/32'], description:'Ração Premier Raças Específicas Shih Tzu para Cães Filhotes'},
+    {id:5, name:'Premier Cookie', img:'img/premier2.jpeg', price:[18.90, 23.90, 27.90], sizes:['1/72', '1/48', '1/32'], description:'Biscoito Premier Cookie para Cães Filhotes 250g'},
+];
